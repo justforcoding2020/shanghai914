3c376530dda4a68f7323856476404ae964c7ae16
Jesse <justfortest@gmail.com>
2023-09-14 11:11:06 +0800

    对控制层接口增加请求方式

--- src/main/java/com/baozige/shanghai914/Controller/admin/UserInfoController.java
+++ src/main/java/com/baozige/shanghai914/Controller/admin/UserInfoController.java
@@ -4,14 +4,12 @@ package com.baozige.shanghai914.Controller.admin;
 import com.baozige.shanghai914.POJO.UserInfo;
 import com.baozige.shanghai914.Service.UserInfoService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.sun.jmx.remote.util.ClassLogger.ok;
 
@@ -22,12 +20,15 @@ import static com.sun.jmx.remote.util.ClassLogger.ok;
 @RequestMapping("admin")
 public class UserInfoController {
     //用户信息增加
+    @PostMapping("admin/auth/add")
     public UserInfo addUserInfo(@RequestBody UserInfo userInfo){
         Assert.notNull(userInfo, "非法输入");
         UserInfoService.insertUserInfo;
         return ok;
     }
     //用户信息删除
+    @ApiOperation(value = "对用户信息进行删除")
+    @DeleteMapping("admin/auth/remove")
     public String removeUserInfo(@RequestParam String number){
         if (StringUtils.isBlank(number)) {
             return "非法删除，请输入具体正确手机号";
