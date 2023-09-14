7c9be92a2d02c2e2c4af65aae1a2c29b68d4bb3f
Jesse <justfortest@gmail.com>
2023-09-14 15:45:39 +0800

    对控制 用户信息接口传参方式修改，增加了token

--- src/main/java/com/baozige/shanghai914/Controller/UserInfoController.java
+++ src/main/java/com/baozige/shanghai914/Controller/UserInfoController.java
@@ -1,4 +1,22 @@
 package com.baozige.shanghai914.Controller;
 
+import com.baozige.shanghai914.POJO.VO.UserInfoVo;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class UserInfoController {
+    //用户信息修改
+    @ApiOperation(value = "对用户信息进行删除")
+    @PutMapping("admin/auth/update")
+    public String removeUserInfo(@RequestParam Integer age, @RequestHeader String token){
+
+        if (StringUtils.isBlank(number)) {
+            return "非法删除，请输入具体正确手机号";
+        }
+        return number;
+    }
 }
