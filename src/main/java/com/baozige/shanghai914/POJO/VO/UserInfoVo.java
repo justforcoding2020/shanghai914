7c03df06d0edd60c96cbc7c1e640a90ef95584ed
Jesse <justfortest@gmail.com>
2023-09-14 11:01:12 +0800

    hotfix分支 增加UserInfoVo List<hobby>属性

--- src/main/java/com/baozige/shanghai914/POJO/VO/UserInfoVo.java
+++ src/main/java/com/baozige/shanghai914/POJO/VO/UserInfoVo.java
@@ -1,4 +1,13 @@
 package com.baozige.shanghai914.POJO.VO;
 
+import javax.swing.*;
+import java.util.List;
+
 public class UserInfoVo {
+    private String name;
+    private String number;
+
+    private Integer age;
+
+    private List<hobby> list;
 }
